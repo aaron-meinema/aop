0240247dc7481ca83c32bd1f25552a636f38c1f1
aaron willem meinema <aaronwillem@hotmail.com>
2019-08-02 13:52:29 +0200

    most lexer and token extending done

--- token/token.go
+++ token/token.go
@@ -1,53 +1,72 @@
 package token
 
 var keywords = map[string]TokenType{
-  "fn": FUNCTION,
-  "let": LET,
+	"fn":        FUNCTION,
+	"let":       LET,
+	"true":      TRUE,
+	"false":     FALSE,
+	"if":        IF,
+	"else":      ELSE,
+	"return":    RETURN,
+	"class":     CLASS,
+	"interface": INTERFACE,
 }
 
 type TokenType string
 
 type Token struct {
-  Type      TokenType
-  Literal   string
+	Type    TokenType
+	Literal string
 }
 
 const (
-  ILLEGAL ="ILLEGAL"
-  EOF="EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-  // Identifiers + literals
-  IDENT ="IDENT"// add, foobar, x, y, ...
-  INT   ="INT"// 1343456
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
 
-  // Operators
-  ASSIGN  = "="
-  PLUS    = "+"
-  MINUS   = "-"
-  BANG    = "!"
-  ASTERISK= "*"
-  SLASH   = "/"
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
 
-  LT      = "<"
-  GT      = ">"
+	LT = "<"
+	GT = ">"
 
-  // Delimiters
+	// Comparison
+	EQ     = "=="
+	NOT_EQ = "!="
 
-  COMMA=","
-  SEMICOLON =";"
-  LPAREN ="("
-  RPAREN =")"
-  LBRACE ="{"
-  RBRACE ="}"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
 
-  // Keywords
-  FUNCTION ="FUNCTION"
-  LET ="LET"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+
+	// My added keywords
+	CLASS     = "CLASS"
+	INTERFACE = "INTERFACE"
 )
 
 func LookupIdent(ident string) TokenType {
-  if tok, ok := keywords[ident]; ok {
-    return tok
-  }
-  return IDENT
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
