2e56a428f843d9267fab69eda6826d588bbb6952
aaron willem meinema <aaronwillem@hotmail.com>
2019-08-02 14:08:56 +0200

    basic repl added

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-  "fmt"
-  "lexer/lexer"
+	"aop/repl"
+	"fmt"
+	"os"
+	"os/user"
 )
 
-func main(){
-  lol := lexer
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hell %s! This is the Aaron Objectoriented Programming language\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
