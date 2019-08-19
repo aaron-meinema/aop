97692f1b770c67bbc39dc38c65abb8833b20cd1b
aaron willem meinema <aaronwillem@hotmail.com>
2019-08-19 17:15:05 +0200

    return parser finished

--- ast/ast.go
+++ ast/ast.go
@@ -31,8 +31,16 @@ type LetStatement struct {
 	Value Expression
 }
 
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,9 +6,37 @@ import (
 	"testing"
 )
 
+func TestRuturnStatements(t *testing.T) {
+	input := `
+	return 5;'
+	return 10;
+	return 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", returnStmt.TokenLiteral())
+		}
+	}
+}
+
 func TestLetStatemetns(t *testing.T) {
 	input := `
-	let x 5;
+	let x = 5;
 	let y = 10;
 	let foobar = 838383;`
 
